f43c8e610a5e1b00c5fb43adbdf196efe067e17c
knalis <vitallot21@gmail.com>
2026-08-24 01:42:34 +0300

    Recover collector mode from existing logs

--- src/cristalix/mod.rs
+++ src/cristalix/mod.rs
@@ -4,4 +4,4 @@ mod tailer;
 
 pub use log_path::{default_latest_log_path, discover_latest_log};
 pub use process::{CristalixProcessDetector, CristalixProcessSnapshot};
-pub use tailer::LogTailer;
+pub use tailer::{LogTailer, scan_existing_log_lines};
