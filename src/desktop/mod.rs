050c99b035eadd7b6449b7e7c2fb4816ae921837
knalis <vitallot21@gmail.com>
2026-08-24 13:49:09 +0300

    Improve Windows DPI scaling

--- src/desktop/mod.rs
+++ src/desktop/mod.rs
@@ -1,3 +1,4 @@
+mod dpi;
 mod mascot;
 mod native_shell;
 mod theme;
@@ -13,9 +14,11 @@ pub struct DesktopLaunchContext {
 }
 
 pub fn run(context: DesktopLaunchContext, runtime: Handle) -> Result<()> {
+    dpi::enable_gdi_scaling_for_thread();
     native_shell::run(context, runtime)
 }
 
 pub fn show_fatal_error(message: &str) {
+    dpi::enable_gdi_scaling_for_thread();
     native_shell::show_fatal_error(message);
 }
