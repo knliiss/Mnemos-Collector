4953b7adea8481cefee73211314fbb56f634b054
knalis <vitallot21@gmail.com>
2026-08-24 03:32:13 +0300

    Expose Cristalix session freshness check

--- src/cristalix/mod.rs
+++ src/cristalix/mod.rs
@@ -1,7 +1,9 @@
 mod log_path;
 mod process;
+mod session;
 mod tailer;
 
 pub use log_path::{default_latest_log_path, discover_latest_log};
 pub use process::{CristalixProcessDetector, CristalixProcessSnapshot};
+pub use session::log_updated_within;
 pub use tailer::{LogTailer, scan_existing_log_lines};
