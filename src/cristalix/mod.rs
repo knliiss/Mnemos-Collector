238972a874bc8bcdff8cc369ee4752d889a129ae
knalis <vitallot21@gmail.com>
2026-08-29 00:26:23 +0300

    Expose configurable Cristalix log source

--- src/cristalix/mod.rs
+++ src/cristalix/mod.rs
@@ -3,7 +3,10 @@ mod process;
 mod session;
 mod tailer;
 
-pub use log_path::{default_latest_log_path, discover_latest_log};
+pub use log_path::{
+    clear_configured_latest_log_path, configured_latest_log_path, default_latest_log_path,
+    discover_latest_log, set_configured_latest_log_path,
+};
 pub use process::{CristalixProcessDetector, CristalixProcessSnapshot};
 pub use session::log_updated_within;
 pub use tailer::{LogTailer, scan_existing_log_lines};
